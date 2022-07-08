580e9f02e6b5836621e37ddc88a27c4a482f97ed
Gary Hake <devgaryhake@gmail.com>
2022-07-08 11:36:35 -0400

    gonna switch to windows what bullshit

---
+++ 01-basics/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nest/core';
+import { Controller, Module, Get } from '@nestjs/common';
+
+@Controller()
+class AppController {
+	@Get()
+	getRootRoute() {
+		return 'Hi there!';
+	}
+}
+
+@Module({
+	controllers: [AppController],
+})
+class AppModule {}
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+
+	await app.listen(3000);
+}
+
+bootstrap();
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# nestjs-bootcamp
+# Nest JS
+
+[Official Docs](https://docs.nestjs.com/)
+
+## Basics
+
+1. Nest
+   ```
+   npm install @nestjs/common @nest/core @nestjs/platform-express reflect-metadata typescript
+   ```
+   - Lots of packages to get nest rolling!
+1. We will be using Express for our http implementation
+1. Reflect-metadata allows us to use decorators
