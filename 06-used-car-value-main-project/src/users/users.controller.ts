18141eb691c4677ed3b37ceaadb8d69bf41385f7
Gary Hake <devgaryhake@gmail.com>
2022-07-11 11:25:28 -0400

    implementing error catching

--- 06-used-car-value-main-project/src/users/users.controller.ts
+++ 06-used-car-value-main-project/src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Delete,
   Param,
   Query,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -22,8 +23,13 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return user;
   }
 
   @Get()
