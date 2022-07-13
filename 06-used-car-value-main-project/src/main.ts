67dc6d34505271b260f062f0bd3807d89d4fb42a
Gary Hake <devgaryhake@gmail.com>
2022-07-13 18:44:54 -0400

    starting session stuff setting cookies when signing up or signing in

--- 06-used-car-value-main-project/src/main.ts
+++ 06-used-car-value-main-project/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,13 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  app.use(
+    session({
+      secret: ['asdfdsfefr'],
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
